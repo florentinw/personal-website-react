2a228e92314e8db3a79a8c431058b0d7047a2951
Florentin <hello@florentin.design>
2019-06-13 22:34:00 +0200

    :mag: Updated Card preview

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,11 +6,13 @@ import CustomLink from "../components/CustomLink";
 import Logo from "../components/Logo";
 import PortfolioLayout from "../components/PortfolioLayout";
 import SocialMediaFooter from "../components/SocialMediaFooter";
+import SEO from "../components/SEO";
 
 class HomePage extends Component {
   render() {
     return (
       <Fragment>
+        <SEO cover={require("../assets/imgs/meta/card-preview.jpg")} />
         <Navbar leftContent={<Logo />} rightContent={<CustomLink to="/about">about</CustomLink>} />
         <Header
           content={`Self-taught, 16-year-old digital product designer and dark chocolate enthusiast focused on creating experiences & beautiful products.`}
